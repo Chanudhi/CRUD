24363376e7dbf09cac6a75180d0c7f3092bd3a13
Chanudhi <aluchanu29@gmail.com>
2024-12-26 14:10:01 +0530

    secondcommit

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("Imma about to raise the roof");
+
+//pw=iCL0ZPNLZZ4v076w
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+//middleware
+
+app.use("/",(req, res,next) =>{
+    res.send("It is working ..yayy");
+})
+
+mongoose.connect("mongodb+srv://admin:iCL0ZPNLZZ4v076w@cluster0.p7sns.mongodb.net/")
+.then(()=> console.log("Connected to MongoDB"))
+.then(()=> {
+    app.listen(5000);
+})
+.catch((err)=> console.log((err)));
