f29708ea06f2c892aebf01eb858f231e8763c81a
Chanudhi <aluchanu29@gmail.com>
2025-03-24 22:55:38 +0530

    Initialize frontend structure with React setup and basic files

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>helow hola
+        <br/>
+        Esto es un ejemplo de código en ReactJS
+      </h1>
+    </div>
+  );
+}
+
+export default App;
